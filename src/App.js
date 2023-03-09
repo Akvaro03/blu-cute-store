90071f23cb82ca45a59ea1a243db451d27bc8edb
Thomicurve <thomigrax@gmail.com>
2023-03-09 09:24:38 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import './App.css';
 
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
-import Home from './pages/home';
-import Products from './pages/products';
+// import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+// import Home from './pages/home';
+// import Products from './pages/products';
 
 function App() {
   return (
-    <div className="App-Header">
-      <Router>
-        <div>
-          <Link className='App-link' style={{ margin: "0 10px"}} to="/">
-            Home
-          </Link>
-          <Link className='App-link' style={{ margin: "0 10px"}} to="/Products">
-            Products
-          </Link>
-        </div>
+    // <div className="App-Header">
+    //   <Router>
+    //     <div>
+    //       <Link className='App-link' style={{ margin: "0 10px"}} to="/">
+    //         Home
+    //       </Link>
+    //       <Link className='App-link' style={{ margin: "0 10px"}} to="/Products">
+    //         Products
+    //       </Link>
+    //     </div>
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Products' element={<Products />} />
-        </Routes>
-      </Router>
+    //     <Routes>
+    //       <Route path='/' element={<Home />} />
+    //       <Route path='/Products' element={<Products />} />
+    //     </Routes>
+    //   </Router>
+    // </div>
+    <div >
+      golaaa
     </div>
   );
 }
