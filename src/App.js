8282fa43ab66b2a8a9d8a2b29800e41ec0906716
Thomicurve <thomigrax@gmail.com>
2023-03-09 08:48:27 -0300

    primer Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from './pages/home';
+import Products from './pages/products';
+
+function App() {
+  return (
+    <div className="App-Header">
+      <Router>
+        <div>
+          <Link className='App-link' style={{ margin: "0 10px"}} to="/">
+            Home
+          </Link>
+          <Link className='App-link' style={{ margin: "0 10px"}} to="/Products">
+            Products
+          </Link>
+        </div>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Products' element={<Products />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
